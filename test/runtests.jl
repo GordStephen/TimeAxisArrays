09d1971bff8a3a33510f17662ce0ea4ec1ed249f
Gord Stephen <gord@gordstephen.ca>
2016-04-27 13:30:28 -0400

    Added readtimeaxisarray

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,31 +3,34 @@ using Base.Test
 using Base.Dates
 
 n = now()
+testwritemime(A::TimeAxisArray) = writemime(IOBuffer(),MIME("text/plain"),A)
 
 data = randn(11)
-A = RegularTimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(8))
+A = TimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(8))
 @test isa(A, RegularTimeAxisArray)
-writemime(IOBuffer(),MIME("text/plain"),A)
+testwritemime(A)
+A2 = TimeAxisArray(randn(5), n-Hour(2):Hour(1):n+Hour(2))
+testwritemime(A2)
 
 data = randn(5,2)
-A = RegularTimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(2),
+A = TimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(2),
                          Axis{:Columns}([:A, :B]))
 @test isa(A, RegularTimeAxisArray)
-writemime(IOBuffer(),MIME("text/plain"),A)
-A2 = RegularTimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(2), [:A, :B])
+testwritemime(A)
+A2 = TimeAxisArray(data, n-Hour(2):Hour(1):n+Hour(2), [:A, :B])
 @test A == A2
 
-data = randn(49,2,3)
-A = RegularTimeAxisArray(data, n-Day(1):Hour(1):n+Day(1),
+data = randn(7,2,3)
+A = TimeAxisArray(data, n-Day(1):Hour(8):n+Day(1),
               Axis{:Columns}([:A, :B]),
               Axis{:Pages}([:I, :II, :III]))
 @test isa(A, RegularTimeAxisArray)
-writemime(IOBuffer(),MIME("text/plain"),A)
+testwritemime(A)
 
 data = randn(49,2,5,3)
-A = RegularTimeAxisArray(data, n-Day(1):Hour(1):n+Day(1),
+A = TimeAxisArray(data, n-Day(1):Hour(1):n+Day(1),
               Axis{:Columns}([:A, :B]),
               Axis{:Pages}([:I, :II, :III, :IV, :V]),
               Axis{:Superpages}([:one, :two, :three]))
 @test isa(A, RegularTimeAxisArray)
-writemime(IOBuffer(),MIME("text/plain"),A)
+testwritemime(A)
