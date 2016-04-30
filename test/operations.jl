19756705bea4028caff4d89b811c768987b60784
Gord Stephen <gord@gordstephen.ca>
2016-04-30 14:32:19 -0400

    Added lead/lag tests

--- test/operations.jl
+++ test/operations.jl
@@ -2,6 +2,8 @@ A1d3 = readtimeaxisarray("1dtest.csv", date(), header=false)
 A1d = TimeAxisArray(randn(10), collect(1.0:10.0))
 A1d[5] = NaN
 A2d = readtimeaxisarray("2dtest.csv", date())
+A4d1 = readtimeaxisarray("4dtest.csv", date(), headlines=3)
+nA4d1 = size(A4d1, 1)
 
 # split
 @test map(size, split(A1d3, week)) == [(3,), (7,), (4,)]
@@ -18,8 +20,20 @@ A2d = readtimeaxisarray("2dtest.csv", date())
 # moving
 
 # lead
+@test lead(A1d1) == TimeAxisArray(A1d1.data[2:end], timestamps(A1d1)[1:end-1])
+@test lead(A1d1) == lead(A1d1, 1)
+@test lead(A1d1, 3) == TimeAxisArray(A1d1.data[4:end], timestamps(A1d1)[1:end-3])
+@test lead(A4d1) == TimeAxisArray(A4d1[Axis{:Timestamp}(2:nA4d1)].data, timestamps(A4d1)[1:end-1], A4d1.axes[2:end]...)
+@test lead(A4d1) == lead(A4d1, 1)
+@test lead(A4d1, 2) == TimeAxisArray(A4d1[Axis{:Timestamp}(3:nA4d1)].data, timestamps(A4d1)[1:end-2], A4d1.axes[2:end]...)
 
 # lag
+@test lag(A1d1) == TimeAxisArray(A1d1.data[1:end-1], timestamps(A1d1)[2:end])
+@test lag(A1d1) == lag(A1d1, 1)
+@test lag(A1d1, 3) == TimeAxisArray(A1d1.data[1:end-3], timestamps(A1d1)[4:end])
+@test lag(A4d1) == TimeAxisArray(A4d1[Axis{:Timestamp}(1:nA4d1-1)].data, timestamps(A4d1)[2:end], A4d1.axes[2:end]...)
+@test lag(A4d1) == lag(A4d1, 1)
+@test lag(A4d1, 2) == TimeAxisArray(A4d1[Axis{:Timestamp}(1:nA4d1-2)].data, timestamps(A4d1)[3:end], A4d1.axes[2:end]...)
 
 # diff
 
