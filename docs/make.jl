dd0b2beaf653099ece475e7c4be52364d1783d3b
Gord Stephen <gord@gordstephen.ca>
2016-06-12 13:16:05 -0400

    Debugging make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,5 @@ makedocs(
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo   = "github.com/GordStephen/TimeAxisArrays.jl.git",
-    julia  = "0.4",
-    osname = "linux"
+    julia  = "0.4"
 )
